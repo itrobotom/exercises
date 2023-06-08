d52af2568550f67dfd3cb2c860d9a4dc58360c00
itrobotom <121325608+itrobotom@users.noreply.github.com>
2023-06-08 15:28:04 +0300

    Добавлена логика на отправку формы
    Сообщение об успешной авторизации и в консоль введенные данные пользователем.

--- #3_React_ModalWin/src/components/ModalReg/StartModal.jsx
+++ #3_React_ModalWin/src/components/ModalReg/StartModal.jsx
@@ -4,11 +4,22 @@ import { InputForm } from '../InputForm/InputForm';
 
 // можно два файла сделать и по флагу через пропсы передвать имя окна, а можно от флага выводить или разметку регистрации, или разметку авторизации как сейчас и сделано
 export function StartModal({ flagRegistr }) {
+    function handleClick(event) {
+        event.preventDefault();
+        console.log(event.target[0].value)
+        for(let i = 0; i < event.target.length; i++) {
+            console.log(event.target[i].value)
+        }
+        console.log(event.target.length);
+        //почему-то два поля считает за 3
+        (event.target.length > 3) ? alert(`Пользолватель ${event.target[0].value} успешно зарегистрирован на сайте`) :
+                                    alert(`Пользолватель ${event.target[0].value} успешно авторизован на сайте`);
+    }
     if (!flagRegistr){
         return (
             <section className='main-container'>
                 <h2>Регистрация</h2>
-                <form>
+                <form onSubmit={handleClick}>
                     <InputForm placeholderForm = "логин" />
                     <InputForm placeholderForm = "почта" />
                     <InputForm placeholderForm = "пароль" />
@@ -21,7 +32,7 @@ export function StartModal({ flagRegistr }) {
         return (
             <section className='main-container'>
                 <h2>Авторизация</h2>
-                <form>     
+                <form onSubmit={handleClick}>     
                     <InputForm placeholderForm = "логин" />
                     <InputForm placeholderForm = "пароль" />       
                     <BtnForm textBtn = "Войти" />
