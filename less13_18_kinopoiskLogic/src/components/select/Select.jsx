aecda4d8317bba462aecc8ae756cdf1d0dd7280a
itrobotom <121325608+itrobotom@users.noreply.github.com>
2023-06-27 16:01:12 +0300

    Добавлены стейты
    Добавлены стейты для хранения параметров фильтров. При этом стейты находятся в родителе, а сами фильтры по отдельности - это отдельные компоненты.

--- less13_18_kinopoiskLogic/src/components/select/Select.jsx
+++ less13_18_kinopoiskLogic/src/components/select/Select.jsx
@@ -1,17 +1,27 @@
 import './Select.css'
 
 
-function SelectSort( {headerSelect, options} ) {
+export function Select( {
+  headerSelect, 
+  options,
+  onChangeSelect
+} ) {
   return (
     <label className="label-filter">
       {headerSelect} 
-      <select className="select-filter" name="sort" id="sort-select">
+      {/* Функция, указанная в атрибуте onChange, будет вызываться каждый раз, когда происходит изменение значения select. 
+      В данном случае, когда происходит изменение выбранного значения select, вызывается анонимная функция (e) => { onChangeSelect(e.target.value) }.
+      Внутри этой анонимной функции e представляет объект события ChangeEvent, которое содержит информацию о событии изменения значения select.  
+      e.target.value представляет выбранное значение select. Далее, вызывается функция onChangeSelect (которая передана из родительского компонента в качестве пропса), 
+      и выбранное значение select передается в качестве аргумента этой функции. То есть, при изменении значения select, вызывается функция 
+      onChangeSelect с выбранным значением в качестве аргумента, чтобы передать это значение обратно в родительский компонент
+      */}
+      <select className="select-filter" name="sort" id="sort-select" onChange={(e) => {onChangeSelect(e.target.value)}}> 
+      {/* лучше индекс массива index не использовать в качестве ключа, а взять имя, т.к. оно не должно повторятья */}
         {options.map((value, index) => (
-          <option key={index} value={value}>{value}</option>
+          <option key={value} value={value}>{value}</option>
         ))}
       </select>
     </label>
   );
 }
-
-export default SelectSort;
--- less13_18_kinopoiskLogic/src/page/moveFilter/MoveFilter.jsx
+++ less13_18_kinopoiskLogic/src/page/moveFilter/MoveFilter.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
 import CheckGenre from "../../components/checkGenre/CheckGenre";
 import Pagination from "../../components/pagination/Pagination";
-import Select from "../../components/select/Select"
+import {Select} from "../../components/select/Select"
 import {categories, years} from '../../data/data'
 import {genre} from '../../data/data'
 import {RequestGenre} from '../../components/Network';
 import './MoveFilter.css';
 
 function MoveFilter() {
-  //1) по идее нам и не надо наверное жанры хранить в переменной состояния?
-  const [genries, setGenries] = useState([]); //храним данные с сервера в genries
-  const [isGetGenre, setGetGenre] = useState(false);
+  const [genres, setGenries] = useState([]); //храним данные с сервера в genries
+  const [isGetGenre, setIsGetGenre] = useState(false);
+  //массив id выделенных элементов галочками (создается на основании всего списка жанров json файла)
+  const [selectedGenresIds, setSelectedGenresIds] = useState([]);
 
+  //переменные состояния хранения селектов фильтров
+  const [selectSort, setSelectSort] = useState('');
+  const [selectYear, setSelectYear] = useState('');
 
   useEffect(() => { //запрос данных только после рендера всей страницы
     RequestGenre()
@@ -22,11 +26,37 @@ function MoveFilter() {
         //Это преобразует объекты в строку JSON с отступами, делая их более читаемыми.
         //2 - количество отступов 
         console.log(JSON.stringify(jsonGenre.genres, null, 2));
-        setGenries(...genries, jsonGenre.genres); //сохранили жанры, пришедшие с сервера
-        setGetGenre(true);
+        setGenries(jsonGenre.genres); //сохранили жанры, пришедшие с сервера
+        setIsGetGenre(true);
       })
   }, []); //пустой массив чтобы перерендер не зациклился
   
+  //создаем и обновляем массив id элементов с выбранными элементами из списка жанров
+  function handleChangeItem(nextGenre){
+    const updatedSelectedIds = [...selectedGenresIds]; //создаем новый массив updatedSelectedIds, который копирует элементы из selectedGenresIds.
+    const index = updatedSelectedIds.indexOf(nextGenre.id); //если элемент не найден, вернется -1
+
+    if (index !== -1) { 
+      updatedSelectedIds.splice(index, 1); // Удалить идентификатор, если уже выбран
+    } else {
+      updatedSelectedIds.push(nextGenre.id); // Добавить идентификатор, если еще не выбран
+    }
+
+    setSelectedGenresIds(updatedSelectedIds);
+    console.log(updatedSelectedIds); // Вывод обновленного массива идентификаторов выбранных жанров
+  }
+
+  function handleChangeSelectSort(selectSort) {
+    setSelectSort(selectSort);
+    console.log(selectSort);
+  }
+
+  function handleChangeSelectYear(selectYear) {
+    setSelectYear(selectYear);
+    console.log(selectYear);
+  }
+
+  //console.log(genres);
   return (
     <div className="filter">
       <h1>Фильтры:</h1>
@@ -34,12 +64,26 @@ function MoveFilter() {
         <button className="button-filter">Сбросить</button>
         <button className="button-filter">Применить</button>
       </div>
-      <Select headerSelect = {'Сортировать по:'} options = {categories}></Select>
-      <Select headerSelect = {'Год релиза:'} options = {years}></Select>
-      {isGetGenre && <CheckGenre genre={genries} />}     
+      <Select headerSelect = {'Сортировать по:'} options = {categories} onChangeSelect={handleChangeSelectSort}></Select>
+      <Select headerSelect = {'Год релиза:'} options = {years} onChangeSelect={handleChangeSelectYear}></Select>
+      {isGetGenre && <CheckGenre 
+        genres={genres}
+        selectedGenresIds={selectedGenresIds} // Передаем selectedGenresIds в CheckGenre
+        onChangeItem={handleChangeItem}
+      />}     
       <Pagination />
     </div>
   );
 }
 
 export default MoveFilter;
+
+// В этом примере используется массив selectedGenresIds, в котором хранятся идентификаторы выбранных жанров. В функции handleChangeItem происходит следующее:
+
+// Создается копия массива selectedGenresIds с помощью оператора расширения [...selectedGenresIds].
+// Проверяется наличие идентификатора жанра в массиве selectedGenresIds с использованием indexOf(nextGenre.id).
+// Если идентификатор уже присутствует в массиве (index > -1), то он удаляется из массива с помощью splice(index, 1).
+// Если идентификатор отсутствует в массиве (index === -1), то он добавляется в конец массива с помощью push(nextGenre.id).
+// Обновленный массив идентификаторов выбранных жанров присваивается состоянию selectedGenresIds с помощью setSelectedGenresIds(updatedSelectedIds).
+// Выводится обновленный массив идентификаторов в консоль для отслеживания изменений.
+// Теперь selectedGenresIds представляет собой массив с выбранными жанрами, и он будет обновляться по мере выделения/снятия выделения чекбоксов.
